d634ac434d75498395444828f917358a16880346
NEUTRON <neutron0831@gmail.com>
2023-02-20 11:19:27 +0900

    :sparkles: Install Vue.js
    - :heavy_plus_sign: Add Vite
    - :heavy_plus_sign: Add Vue Router
    - :heavy_plus_sign: Add Pinia
    - :heavy_plus_sign: Add Vuetify
    - :heavy_plus_sign: Add ESLint
    - :heavy_plus_sign: Add Prettier
    - :see_no_evil: Add `.gitignore`

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true
+}
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  //
+</script>
+
+<template>
+  <VAppBar flat>
+    <VAppBarTitle>
+      <VIcon icon="mdi-circle-slice-6" />
+
+      Essentials Preset
+    </VAppBarTitle>
+  </VAppBar>
+</template>
