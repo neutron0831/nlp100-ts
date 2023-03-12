f2e4805a48b2abafb156426da3ff6f695c28b382
NEUTRON <neutron0831@gmail.com>
2023-03-12 19:08:19 +0900

    :white_check_mark: Add Exercise 01 test (#1)

---
+++ src/chapters/01/test/ex01.test.ts
@@ -0,0 +1,7 @@
+import { ex01 } from '../ex01'
+
+describe('01. "schooled"', () => {
+  it('the 1st, 3rd, 5th, and 7th letters in the string "schooled"', () => {
+    expect(ex01()).toBe('shoe')
+  })
+})
